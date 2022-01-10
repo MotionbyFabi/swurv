8d8ca96f904a09c2e9e891ec8174599dbb317d67
Devecreative <devecreative@gmail.com>
2022-01-10 18:08:02 +0100

    updated about component

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -12,30 +12,30 @@ const About = () => {
 
 
             <div class="wrapper">
-                <div id="left" class="box center" style="background-image: url('../../assets/quote-image-1.png');">
+                <div id="left" class="box center">
                     <p class="quote">"​Really the best motion designer to work with. Can pretty much make everything you ask for and can adapt to your own wishes, also afterwards. Super reliable, gets everything done in time and most importantly really nice and easy to work with!"</p>
                     <p class="name">GOSSE</p>
                     <p class="name">@gossefn</p>
                 </div>
-                <div id="right" class="box"/>
+                <div id="right" class="box quote-img-1"/>
             </div>  
 
             <div class="wrapper">
-                <div id="left" class="box center" style="background-image: url('../../assets/quote-image-2.png');" >
+                <div id="left" class="box center">
                     <p class="quote">"Great professional service, I enjoyed working with Swurv. He proved himself to be reliable and consistent with great communication."</p>
                     <p class="name">MARSHALL</p>
                     <p class="name">@marshallbuick</p>
                 </div>
-                <div id="right" class="box"/>
+                <div id="right" class="box quote-img-2"/>
             </div>  
 
             <div class="wrapper">
-                <div id="left" class="box center" style="background-image: url('../../assets/quote-image-3.png');" >
+                <div id="left" class="box center" >
                     <p class="quote">"​Good quality service. Swurv offered to remake an outro for Bucke and did an amazing job with it. He worked fast and got it done with high quality."</p>
                     <p class="name">ALBO</p>
                     <p class="name">@albogravity</p>
                 </div>
-                <div id="right" class="box"/>
+                <div id="right" class="box quote-img-3"/>
             </div>  
 
             
