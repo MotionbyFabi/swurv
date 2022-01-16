8b3cc84ef2098a09078306a2a23ff194448f85ac
Fabio Di Nota <fabiodinota@gmail.com>
2022-01-16 11:42:26 +0100

    image meta tag fix

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -1,12 +1,22 @@
 import react from "react";
 import './work.css';
+import ReactPlayer from "react-player"
+import Lesson from "./player"
 
 
-
-
-const Work = () => {
+const Work = ({url, onProgress}) => {
       return (
-       <h1>hello</h1>
+        <div className="player-wrapper">
+        <ReactPlayer
+          className="react-player"
+          url={url}
+          width="100%"
+          height="100%"
+          controls={true}
+          onProgress={onProgress}
+        />
+        <Lesson />
+      </div>
     );
 };
 
