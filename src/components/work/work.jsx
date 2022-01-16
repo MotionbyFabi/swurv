b34e14903bb731d73ea433368c7f8f09186f3a21
Fabio Di Nota <fabiodinota@gmail.com>
2022-01-16 11:46:54 +0100

    hopeful fix of last commit

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -1,21 +1,12 @@
 import react from "react";
 import './work.css';
-import ReactPlayer from "react-player"
-import Lesson from "./player"
+
 
 
 const Work = ({url, onProgress}) => {
       return (
-        <div className="player-wrapper">
-        <ReactPlayer
-          className="react-player"
-          url={url}
-          width="100%"
-          height="100%"
-          controls={true}
-          onProgress={onProgress}
-        />
-        <Lesson />
+        <div>
+            <h1>hello work tab</h1>
       </div>
     );
 };
