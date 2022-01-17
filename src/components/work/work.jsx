c3bcd351434afc95441acdc1d8ee957781e5341c
Fabio Di Nota <fabiodinota@gmail.com>
2022-01-17 17:53:15 +0100

    work changes

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -6,7 +6,8 @@ import './work.css';
 const Work = ({url, onProgress}) => {
       return (
         <div>
-            <iframe src="https://player.vimeo.com/video/581632727?h=7b31e540d6" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+          <iframe src="https://player.vimeo.com/video/581632727?h=7b31e540d6" width="1000" height="562,50" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+          <iframe src="https://player.vimeo.com/video/581633680?h=a8a113cc94" width="1000" height="562,50" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
       </div>
     );
 };
