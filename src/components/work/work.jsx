a7cd92a35d75b52243caa87ba1f91c97d7031fad
Fabio Di Nota <fabiodinota@gmail.com>
2022-01-27 15:46:45 +0100

    Hamburger menu

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -8,8 +8,8 @@ const Work = () => {
       return (
             <AnimatedPage className="wrapper">
             
-              <iframe src="https://player.vimeo.com/video/581632727?h=7b31e540d6" width="1000" height="562,50" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-              <iframe src="https://player.vimeo.com/video/581633680?h=a8a113cc94" width="1000" height="562,50" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+              <iframe className="first" src="https://player.vimeo.com/video/581632727?h=7b31e540d6" width="1000" height="562,50" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+              <iframe className="second"  src="https://player.vimeo.com/video/581633680?h=a8a113cc94" width="1000" height="562,50" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
               
           </AnimatedPage>
     );
