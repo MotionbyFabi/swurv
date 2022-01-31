9dfd8ae1841abe68576ba45fa041a90a7cd20f11
Fabio Di Nota <fabiodinota@gmail.com>
2022-01-31 20:42:54 +0100

    import fix I hope

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,4 +1,5 @@
 import React, { Component }  from "react";
+import ReactDOM from 'react-dom'
 import './navigation.css';
 import logoanim from './logoanimsmall.gif'
 import twitter from './twitter.png'
